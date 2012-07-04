0cc86a5ba899f6db1499849ac0fe732f9a0c5b4b
Jorge Bastida <me@jorgebastida.com>
2012-07-04 21:19:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='glue',
-    version='0.2.5',
+    version='0.2.6',
     url='http://github.com/jorgebastida/glue',
     license='BSD',
     author='Jorge Bastida',
