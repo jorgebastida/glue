ff94215ce1017219393c40829c8fc91a1f10fda1
Jorge Bastida <me@jorgebastida.com>
2013-01-27 19:46:04 +0000

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'glue'
-copyright = u'2012, Jorge Bastida'
+copyright = u'2013, Jorge Bastida'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
