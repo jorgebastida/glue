557e6dd46b8c52cd80c7fa4219b30a544280cd77
b <a@b.com>
2015-05-22 09:55:41 +0200

    added a Phaser.js compatible JSON output format

--- glue/formats/__init__.py
+++ glue/formats/__init__.py
@@ -4,9 +4,9 @@ from .img import ImageFormat
 from .html import HtmlFormat
 from .jsonformat import JSONFormat
 from .caat import CAATFormat
+from .phaser import PhaserFormat
 from .less import LessFormat
 from .scss import ScssFormat
-
 
 formats = {'css': CssFormat,
            'cocos2d': Cocos2dFormat,
@@ -14,5 +14,6 @@ formats = {'css': CssFormat,
            'html': HtmlFormat,
            'json': JSONFormat,
            'caat': CAATFormat,
+           'phaser': PhaserFormat,
            'less': LessFormat,
            'scss': ScssFormat}
